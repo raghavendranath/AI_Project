b5f51ee8970d3f37268fc1e3e5b16dd1213896e5
Jacob Roschen <jroschen@gmail.com>
2017-04-29 12:56:09 -0500

    Added some commments to GridUtility

--- src/main/scala/GridUtility.scala
+++ src/main/scala/GridUtility.scala
@@ -6,33 +6,43 @@ object GridUtility {
     println(x)
     x
   }
-  def lineEq(p1: Point, p2: Point): lineeq ={
+
+  // Creates a line equation from two points
+  def lineEq(p1: Point, p2: Point): lineeq = {
     val xcor = p2.y - p1.y
     val ycor = -(p2.x - p1.x)
     val cons = p1.x * (p2.x - p1.y) - p1.y * (p2.x - p1.x)
+
     lineeq(xcor,ycor,cons)
   }
+
+  // Checks to see if a line is parallel to another
   def isParallel(l1: lineeq, l2: lineeq): Boolean ={
     val x = l2.ycoeff * l1.xcoeff
     val y = l2.xcoeff * l1.ycoeff
+
     x == y
-    
   }
+
+  // Returns the point in which the two lines intersect
+  // Note: Call isParallel to make sure that the lines intersect first
   def interPoint(l1: lineeq, l2: lineeq): Point = {
     val x = l2.ycoeff * l1.c - l1.ycoeff * l2.c
     val y = l1.xcoeff * l2.c - l2.xcoeff * l1.c
     val div = l2.ycoeff * l1.xcoeff - l2.xcoeff * l1.ycoeff
     val xcor = x / div
     val ycor = y / div
+
     new Point(xcor.toInt, ycor.toInt)
-    
   }
+
   //to check if it is present one the side of a polygon
   //(side: a,b and intersection x) -> (ax+xb = ab)
   def isValid(p1: Point, p2: Point,p3: Point): Boolean ={
     val x = distance(p1, p3)
     val y = distance(p2, p3)
     val z = distance(p1, p2)
+
     (x + y) == z
   }
   
