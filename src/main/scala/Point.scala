6b0c133522a711a0c1157eb397a1c8c6abdf2f6c
Jacob Roschen <jroschen@gmail.com>
2017-04-30 01:15:44 -0500

    Fixed an issue with the Point class equality

--- src/main/scala/Point.scala
+++ src/main/scala/Point.scala
@@ -1,14 +1,10 @@
 
-class Point(val x: Int, val y: Int) {
+class Point(val x: Double, val y: Double) {
   override def toString: String =  s"($x, $y)"
-
-  override def hashCode(): Int = {
-    31 * x + y
-  }
 
   override def equals(obj: scala.Any): Boolean = {
     obj match {
-      case that: Point => that.isInstanceOf[Point] && this.hashCode == that.hashCode
+      case that: Point => this.x == that.x && this.y == that.y
       case _ => false
     }
   }
